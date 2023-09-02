3a4ac141669df9d1b5b1df7d1158a3045eb7a4ad
Adhiban05 <110900816+Adhiban05@users.noreply.github.com>
2023-09-02 15:14:51 +0530

    nothing

--- Name_tuple.py
+++ Name_tuple.py
@@ -3,4 +3,4 @@ from collections import namedtuple
 Person = namedtuple('Person', ['name', 'age'])
 person = Person(name='Person1', age=25)
 
-print(person.name, person.age)  # Output: Alice 25
+print(person.name, person.age)  # Output: Person1 25
